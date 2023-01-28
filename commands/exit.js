e26c0714a63022aa0b9a69807ac7950f0ad1c5c1
Burak Çakmaktaş <75331562+BuraKKuChakmaktashu@users.noreply.github.com>
2023-01-28 21:10:27 +0300

    Add files via upload

---
+++ commands/exit.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const {MessageEmbed } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName("exit")
+        .setDescription("Kick the bot from the channel."),
+	execute: async ({ client, interaction }) => {
+
+        // Get the current queue
+		const queue = client.player.getQueue(interaction.guildId);
+
+		if (!queue)
+		{
+			await interaction.reply("There are no songs in the queue");
+			return;
+		}
+
+        // Deletes all the songs from the queue and exits the channel
+		queue.destroy();
+
+        await interaction.reply("May your woes be many... and your days few...");
+	}
+}
